7c9b480c3be54ae8cbe2b09ae0ca673370f5a915
Brandon Awan <brandon@axumtechnologies.com>
2022-07-19 17:04:20 +0100

    updated the port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 
 app.get("/", function(req, res){
@@ -43,8 +43,8 @@ app.post("/", function(req, res){
     })
 });
 
-app.listen(3000, function(){
-	console.log("Server started on port 3000");
+app.listen(port, function(){
+    console.log("Server started on port 3000");
 });
 
 
